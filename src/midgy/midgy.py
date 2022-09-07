83d4804a6c17ed4a5e22ed7f132c3989f20fb49f
tonyfast <tony.fast@gmail.com>
2022-09-07 14:36:02 -0700

    streaming transpiler

--- src/midgy/midgy.py
+++ src/midgy/midgy.py
@@ -1,9 +1,7 @@
-from cmath import tan
-from dataclasses import dataclass
 from io import StringIO
 from pathlib import Path
 from re import compile
-from textwrap import dedent, indent
+from textwrap import indent
 
 from markdown_it import MarkdownIt
 
@@ -11,31 +9,27 @@ BLANK, CONTINUATION, COLON, FENCE, SPACE = "", "\\", ":", "```", " "
 QUOTES = "'''", '"""'
 CELL_MAGIC, DOCTEST_LINE = compile("^\s*%{2}\S"), compile("^\s*>{3}\s+")
 DOCTEST_CHAR, CONTINUATION_CHAR, COLON_CHAR, QUOTES_CHARS = 62, 92, 58, {39, 34}
-_RE_BLANK_LINE = compile(r"^\s*\r?\n")
-
-
-def field(default=None, description=None, **metadata):
-    from dataclasses import field
-
-    param = {callable(default) and "default_factory" or "default": default}
-    if description:
-        metadata["description"] = description
-    return field(metadata=metadata or None, **param)
-
+DOCTEST_CHARS = DOCTEST_CHAR, DOCTEST_CHAR, DOCTEST_CHAR
 
 def doctest(state, startLine, end, silent=False):
     """a markdown-it-py plugin for doctests
 
     doctest are a literate programming convention in python that we
     include in the pidgy grammar. this avoids a mixing python and doctest
-    code together."""
-    start = state.sCount[startLine]
+    code together.
 
-    # if it's indented more than 3 spaces, it should be a code block
-    if (start - state.blkIndent) >= 4:
+    the doctest blocks:
+    * extend the indented code blocks
+    * do not conflict with blockquotes
+    * are implicit code fences with the `pycon` info
+    * can be replaced with explicit code blocks.
+    """
+    start = state.bMarks[startLine] + state.tShift[startLine]
+
+    if (start - state.blkIndent) < 4:
         return False
 
-    if all(map(DOCTEST_CHAR.__eq__, state.srcCharCode[start : start + 3])):
+    if state.srcCharCode[start : start + 3] == DOCTEST_CHARS:
         indent, next = state.sCount[startLine], startLine + 1
         while next < end:
             if state.isEmpty(next):
@@ -43,12 +37,13 @@ def doctest(state, startLine, end, silent=False):
             if state.sCount[next] < indent:
                 break
             begin = state.sCount[next]
-            if all(map(DOCTEST_CHAR.__eq__, state.srcCharCode[begin : begin + 3])):
+            if state.srcCharCode[begin : begin + 3] == DOCTEST_CHARS:
                 break
             next += 1
 
         state.line = next
-        token = state.push("doctest", "code", 0)
+        token = state.push("fence", "code", 0)
+        token.info = "pycon"
         token.content = state.getLines(startLine, next, 0, True)
         token.map = [startLine, state.line]
         return True
@@ -57,49 +52,41 @@ def doctest(state, startLine, end, silent=False):
 
 def code(state, start, end, silent=False):
     if state.sCount[start] - state.blkIndent >= 4:
-        leading, next, quoted, colon = 0, start, False, False
+        first_indent, last_indent, next, last_line = 0, 0, start, start
         while next < end:
             if state.isEmpty(next):
                 next += 1
                 continue
-            here = state.sCount[next]
-            if all(map(DOCTEST_CHAR.__eq__, state.srcCharCode[here : here + 3])):
-                break
-            elif state.sCount[next] - state.blkIndent >= 4:
-                if not leading:
-                    leading = state.sCount[next]
-                trailing = state.sCount[next]
-                last_char = state.eMarks[next] - 1
-                continued = state.srcCharCode[last_char] == CONTINUATION_CHAR
-                length = state.eMarks[next] - (state.sCount[next])
-                if continued:
-                    last_char -= 1
-                if continued and quoted and length == 1:
-                    pass
-                else:
-                    colon = state.srcCharCode[last_char] == COLON_CHAR
-                    quoted = state.srcCharCode[last_char] in QUOTES_CHARS
-                    if quoted:
-                        quoted = (
-                            state.srcCharCode[last_char - 2 : last_char]
-                            == (state.srcCharCode[last_char],) * 2
-                        )
-                    else:
-                        quoted = False
-                next = last = next + 1
+            if state.sCount[next] - state.blkIndent >= 4:
+                begin = state.bMarks[next] + state.tShift[next]
+                if state.srcCharCode[begin : begin + 3] == DOCTEST_CHARS:
+                    break
+                if not first_indent:
+                    first_indent = state.sCount[next]
+                last_indent, last_line = state.sCount[next], next
+                next += 1
             else:
                 break
-        state.line = last
+        state.line = next
         token = state.push("code_block", "code", 0)
-        token.content = state.getLines(start, last, 4 + state.blkIndent, True)
+        token.content = state.getLines(start, next, 4 + state.blkIndent, True)
         token.map = [start, state.line]
-        token.meta.update(
-            leading=leading,
-            trailing=trailing,
-            continued=continued,
-            colon=colon,
-            quotes=quoted,
+        end_char = state.srcCharCode[state.eMarks[last_line] - 1]
+        meta = dict(
+            quoted_block=False,
+            colon_block=False,
+            first_indent=first_indent,
+            last_indent=last_indent,
         )
+        if end_char == CONTINUATION_CHAR:
+            end_char = state.srcCharCode[state.eMarks[last_line] - 2]
+        if end_char == COLON_CHAR:
+            meta["colon_block"] = True
+        elif end_char in QUOTES_CHARS and end > 2:
+            meta["quoted_block"] = all(
+                end_char.__eq__, state.srcCharCode[end - 3 : end]
+            )
+        token.meta.update(meta)
         return True
     return False
 
@@ -113,16 +100,15 @@ class Renderer(MarkdownIt):
         self.block.ruler.before("code", "doctest", doctest)
         self.block.ruler.disable("code")
         self.block.ruler.after("doctest", "code", code)
-
-        # self.block.ruler.before("code", "doctest", doctest)
-        # instantiate the renderer_cls
         self.renderer_cls, self.renderer = renderer, renderer()
+
+    def initial_env(self, src):
+        return dict(source=StringIO(src), target=StringIO(), last_line=0)
 
     def parse(self, src, env=None):
         """parse the source and return the markdown tokens"""
         if env is None:
-            env = dict(vars(Env(source=StringIO(src))))
-
+            env = self.initial_env(src)
         return super().parse(src, env)
 
     def render(self, src, env=None):
@@ -130,8 +116,7 @@ class Renderer(MarkdownIt):
         if CELL_MAGIC.match(src):
             return src
         if env is None:
-            env = dict(vars(Env(source=StringIO(src))))
-
+            env = self.initial_env(src)
         return super().render(src, env)
 
     def render_lines(self, src):
@@ -142,24 +127,25 @@ class Renderer(MarkdownIt):
 class Markdown:
     """a renderer_cls to be used with a Renderer"""
 
+    def generic_lines(self, env, next=None):
+        yield from self.yieldlines(next.map[0], env) if next else env["source"]
+
     def generic(self, env, next=None):
-        return (
-            "".join(env["source"]) if next is None else self.readlines(next.map[0], env)
-        )
+        return "".join(self.generic_lines(env, next))
 
     def walk(self, tokens, options, env):
         for token in tokens:
             if hasattr(self, token.type):
-                yield self.generic(env, token)
                 yield getattr(self, token.type)(token, options, env)
         else:
             yield self.generic(env)
 
     def render(self, tokens, options, env):
-        body = StringIO()
         for block in self.walk(tokens, options, env):
-            body.writelines(block)
-        return body.getvalue()
+            print(block, file=env["target"], end="")
+        else:
+            print(self.generic(env), file=env["target"], end="")
+        return env["target"].getvalue()
 
     def readline(self, env):
         try:
@@ -167,62 +153,11 @@ class Markdown:
         finally:
             env["last_line"] += 1
 
-    def readlines(self, stop, env):
+    def yieldlines(self, stop, env):
         """read multiple lines until you want to stop"""
-        s = StringIO()
         while env["last_line"] < stop:
-            s.writelines(self.readline(env))
-        return s.getvalue()
+            yield self.readline(env)
 
-
-@dataclass
-class Env:
-    """the rendering environment for the markdown it rendered"""
-
-    @dataclass
-    class IndentState:
-        """the indent state relative to a markdown block    .
-
-        the diagram below show the refenerce, trailing, and leading indents relative
-        to a markdown block. the indents for the markdown need to be computed relative
-        to the enclosing code blocks.
-
-        reference CODECODECODECODECODECODECODECODE
-        trailing  CODECODECODECODECODECODECODECODE
-
-        MMMMMMDDDDDDDDDMMMMMMDDDDDDDDDMMMMMMDDDDDDDDD
-        MMMMMMDDDDDDDDDMMMMMMDDDDDDDDDMMMMMMDDDDDDDDD
-
-        leading        CODECODECODECODECODECODECODE
-                CODECODECODECODECODECODECODE"""
-
-        reference: int = field(
-            0, "The first indent of the first line of code in a document"
-        )
-        trailing: int = field(0, "The indent of last code line before a markdown block")
-        leading: int = field(0, "The indent of last code line before a markdown block")
-
-    @dataclass
-    class LastCharacterState:
-        """the first non-blank character before a markdown block can change how the
-        rendering step behaves. these conditions let us define markdown in python
-        variables and write docstrings with markdown"""
-
-        colon: bool = field(False, "The code before the markdown ends with `:`")
-        quotes: bool = field(
-            False, "The code before the markdown ends with triple quotes"
-        )
-
-    source: StringIO = field(None, "input code being translated")
-    last_line: int = field(0, "the last code line visited")
-    indents: IndentState = field(IndentState)
-    chars: LastCharacterState = field(LastCharacterState)
-    terminal_character: str = field(
-        BLANK, "trailing character after the non-code block"
-    )
-
-
-DOCTEST_CHAR, CONTINUATION_CHAR, COLON_CHAR, QUOTES_CHARS = 62, 92, 58, {39, 34}
 
 # an instance of this class is used to transform markdown to valid python
 # in the ipython extension. the python conversion is constrained by being
@@ -236,103 +171,83 @@ class Python(Markdown):
 
     markdown_is_block_string = True
     docstring_block_string = True
+    quote_char = '"'
 
-    def indent(self, input, i=0, prefix=""):
-        """indent an input"""
-        return indent(input, SPACE * i + prefix)
-
-    def quote(self, input, env, i=0):
-        """quote a string which requires the environment state"""
-        return self._get_quoted(input, i, env)
-
-    def comment(self, input, i=0):
-        """comment an input"""
-
-        return self.indent(input, i, "# ")
-
-    def get_noncode_indent(self, env):
-        return (
-            self._get_noncode_indent(env["indents"], env["chars"])
-            - env["indents"].reference
-        )
-
-    # this method is defined by the reusable markdown parent
     def generic(self, env, next=None):
-        """process a generic block of markdown text."""
+        body = super().generic(env, next)
 
-        indents, chars = env["indents"], env["chars"]
-        if next is None:
-            env["terminal_character"] = ";"
+        ref = env.get("reference_indent", 0)
 
-        else:
-            if not indents.reference:
-                indents.trailing = indents.reference = next.meta["leading"]
-            indents.leading = next.meta["leading"]
+        if env.get("quoted_block"):
+            return indent(body, SPACE * ref)
 
-        input, indent = super().generic(env, next), self.get_noncode_indent(env)
+        left = body.rstrip()
 
-        if chars.quotes:
-            # when we find quotes in code around a markdown block we don't augment the string.
-            input = self.indent(input, indent)
-        elif self.markdown_is_block_string:
-            # augment a non-code markdown block as quoted string
-            input = self.quote(input, env, indent)
-        else:
-            input = self.comment(input, indent)
+        if not left:
+            return body
 
-        return self.indent(input, indents.reference)
+        # get states of the enclosing code blocks if there are any
+        next_indent = next.meta["first_indent"] if next else 0
+        prior_indent = env.get("last_indent", 0)
+        colon = env.get("colon_block", False)
+        spaces = prior_indent # compute extra spaces to apply
+
+        if colon:
+            # this is a python convention we apply when inside of the a colon block.
+            if next_indent > prior_indent:
+                # when the trailing indent is greater we prefer that
+                spaces = next_indent
+            else:
+                # add post colon default spaces.
+                spaces += 4
+
+        # we're going to shuffle from lines to generic
+        lines, generic = StringIO(left), StringIO()
+
+        # this loop breaks at the first populated line.
+        # it indents and quotes the first line then breaks.
+        for line in lines:
+            any = bool(line.lstrip())
+            if any:
+                print(SPACE * (spaces - ref), file=generic, end="")
+                print(self.quote_char * 3, file=generic, end="")
+            print(line, file=generic, end="")
+            if any:
+                break
+
+        # add the rest of the lines to the generic bufffer
+        # up until the last whitespace.
+        for line in lines:
+            print(line, file=generic, end="")
+
+        # insert quotes before the white space
+        print(self.quote_char * 3, file=generic, end="")
+        if not next:
+            # add a semi colon to the last string block to suppress that strings output
+            print(";", file=generic, end="")
+
+        # add the rest of the white space
+        print(body[len(left) :], file=generic, end="")
+
+        return generic.getvalue()
 
     def code_block(self, token, options, env):
-        """update the state block when code is found."""
-        indents, chars = env["indents"], env["chars"]
+        body = StringIO()
+        # set the default reference indent as soon as we can.
+        ref = env.setdefault("reference_indent", token.meta["first_indent"])
 
-        # update the trailing indent
-        indents.trailing = token.meta["trailing"]
+        # add the prior block of markdown
+        print(self.generic(env, token), end="", file=body)
 
-        # measure if there is a preceding colon indicating a python block indent
-        chars.colon = token.meta["colon"]
+        for line in self.yieldlines(token.map[1], env):
+            print(line[ref:], end="", file=body)
 
-        # measure if triple quotes exist around the surround code block
-        chars.quotes = token.meta["quotes"]
-
-        # read the lines pertaining to the raw code.
-        return self.readlines(token.map[1], env)
-
-    def render(self, tokens, options, env):
-        return dedent(super().render(tokens, options, env))
-
-    @staticmethod
-    def _get_noncode_indent(indents, chars, **_):
-        """compute the current indent based on the enclosing blocks"""
-        if chars.quotes:  # then we require no adjustment
-            return indents.reference
-        elif chars.colon:
-            return max(indents.trailing + 4, indents.leading)
-        elif indents.leading >= indents.trailing:
-            return indents.leading
-        return indents.trailing
-
-    @staticmethod
-    def _get_quoted(input, indent=0, env=None):
-        """heuristics that quote a non-code block as a string."""
-
-        input = dedent(input)
-        quote = QUOTES[QUOTES[0] in input]
-        l, r = input.lstrip(), input.rstrip()
-        if not (l or r):  # we have a blank string
-            return input
-        begin, end = input[: len(input) - len(l)], input[len(r) :]
-        return (  # recombine all of the parts into quoted python
-            begin  # leading whitespace1
-            + SPACE * indent  # computed indent
-            + quote  # enter block string
-            + get_escaped_string(
-                input[len(input) - len(l) : len(r)], quote[0]
-            )  # code body
-            + quote  # exit block string
-            + env["terminal_character"]  # computed trailing character
-            + end  # trailing whitespace
+        env.update(
+            colon_block=token.meta["colon_block"],
+            quoted_block=token.meta["quoted_block"],
+            last_indent=token.meta["last_indent"],
         )
+        return body.getvalue()
 
 
 def md_to_python(body, renderer_cls=Python, *, _renderers={}):
@@ -356,7 +271,6 @@ def iter_globs(*glob, recursive=False):
 
     for g in glob:
         yield from map(Path, find(g))
-            
 
 
 def format_black(body):
@@ -391,15 +305,16 @@ def show_rich(source, md=False, py=True, format=False, **kwargs):
             lineno = True
         columns.append(Panel(Syntax(py, "python", line_numbers=lineno), title="py"))
 
-    print(Columns(columns,   **kwargs))
+    print(Columns(columns, **kwargs))
+
 
 def main():
     from sys import argv
-    
+
     for path in iter_globs(*argv[1:]):
         show_rich(path.read_text(), md=True, format=True, title=str(path))
 
-    
+
 def arguments():
     from argparse import ArgumentParser
 
@@ -448,6 +363,7 @@ def get_escaped_string(object, quote='"'):
     from re import subn
 
     return subn(r"%s{1,1}" % quote, "\\" + quote, object)[0]
+
 
 if __name__ == "__main__":
     main()
