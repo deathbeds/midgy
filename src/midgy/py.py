2947c0376ead9284ed4e18b416aebd8b3f31942f
tonyfast <tony.fast@gmail.com>
2022-09-09 17:12:18 -0700

    update py src

--- src/midgy/py.py
+++ src/midgy/py.py
@@ -1,89 +1,75 @@
-from .md import Markdown, Renderer, SPACE
-from textwrap import indent
+"""render markdown as python code
+
+this module exports the renderer class to transform
+markdown into python using midgy conventions."""
+
+from functools import partial
+from .md import Markdown, MarkdownIt, SPACE
 from io import StringIO
 from re import compile
 
 ESCAPE = {x: "\\" + x for x in "'\""}
 ESCAPE_PATTERN = compile("[" + "".join(ESCAPE) + "]")
 
-
-def escape(x, char='"'):
-    def match(m):
-        return ESCAPE.get(m.group(0))
-
-    return ESCAPE_PATTERN.sub(match, x)
+escape = partial(ESCAPE_PATTERN.sub, lambda m: ESCAPE.get(m.group(0)))
 
 
-# an instance of this class is used to transform markdown to valid python
-# in the ipython extension. the python conversion is constrained by being
-# a line for line transformation using indent code blocks (not code fences)
-# as references for translating the markdown to valid python objects.
-
-# the choice of indented code over code fences allows for more implicit interleaving
-# of code and narrative.
-class Python(Markdown):
-    """a line-for-line markdown to python renderer"""
-
+class Config:
     markdown_is_block_string = True
     docstring_block_string = True
     quote_char = '"'
+    formatter = None
+
+
+class Python(Markdown, Config):
+    """a line-for-line markdown to python renderer"""
+
+    def compose_generic_string(self, body, env, lead="", pre="", trail=""):
+        """compose a block string.
+        lead applies to first line, pre applies to every other line,"""
+
+        left = body.rstrip()
+        if not left:
+            return body
+        old, new = StringIO(left), StringIO()
+        if lead:
+            # this loop breaks at the first populated line.
+            # it indents and quotes the first line then breaks.
+            for line in old:
+                any = bool(line.lstrip())
+                any and print(lead, file=new, end="")
+                print(escape(line), file=new, end="")
+                if any:
+                    break
+
+        for line in old:
+            print(pre, escape(line), file=new, end="", sep="")
+        print(trail, body[len(left) :], file=new, end="")
+        return new.getvalue()
+
+    def compute_generic_lead(self, env, next=None):
+        next_indent = next.meta["first_indent"] if next else 0
+        spaces = prior_indent = env.get("last_indent", 0)
+        if env.get("colon_block", False):  # inside a python block
+            if next_indent > prior_indent:
+                spaces = next_indent  # prefer greater trailing indent
+            else:
+                spaces += 4  # add post colon default spaces.
+        return SPACE * (spaces - env.get("reference_indent", 0))
 
     def generic(self, env, next=None):
         body = super().generic(env, next)
-
-        ref = env.get("reference_indent", 0)
-
         if env.get("quoted_block"):
-            return indent(body, SPACE * ref)
-
-        left = body.rstrip()
-
-        if not left:
             return body
-
-        # get states of the enclosing code blocks if there are any
-        next_indent = next.meta["first_indent"] if next else 0
-        prior_indent = env.get("last_indent", 0)
-        colon = env.get("colon_block", False)
-        spaces = prior_indent  # compute extra spaces to apply
-
-        if colon:
-            # this is a python convention we apply when inside of the a colon block.
-            if next_indent > prior_indent:
-                # when the trailing indent is greater we prefer that
-                spaces = next_indent
-            else:
-                # add post colon default spaces.
-                spaces += 4
-
-        # we're going to shuffle from lines to generic
-        lines, generic = StringIO(left), StringIO()
-
-        # this loop breaks at the first populated line.
-        # it indents and quotes the first line then breaks.
-        for line in lines:
-            any = bool(line.lstrip())
-            if any:
-                print(SPACE * (spaces - ref), file=generic, end="")
-                print(self.quote_char * 3, file=generic, end="")
-            print(escape(line), file=generic, end="")
-            if any:
-                break
-        # add the rest of the lines to the generic bufffer
-        # up until the last whitespace.
-        for line in lines:
-            print(escape(line), file=generic, end="")
-
-        # insert quotes before the white space
-        print(self.quote_char * 3, file=generic, end="")
+        trail = self.quote_char * 3
+        lead = self.compute_generic_lead(env, next) + trail
         if not next:
-            # add a semi colon to the last string block to suppress that strings output
-            print(";", file=generic, end="")
+            trail += ";"
+        return self.compose_generic_string(body, env, lead=lead, trail=trail)
 
-        # add the rest of the white space
-        print(body[len(left) :], file=generic, end="")
-
-        return generic.getvalue()
+    def doctest(self, token, options, env):
+        if self.docstring_block_string:
+            return
 
     def shebang(self, token, options, env):
         return "".join(self.yieldlines(token.map[1], env))
@@ -95,7 +81,6 @@ class Python(Markdown):
 
         # add the prior block of markdown
         print(self.generic(env, token), end="", file=body)
-
         for line in self.yieldlines(token.map[1], env):
             print(line[ref:], end="", file=body)
 
@@ -107,23 +92,16 @@ class Python(Markdown):
         return body.getvalue()
 
     def front_matter(self, token, options, env):
-        pre = env.setdefault("pre", StringIO())
-        start, stop = token.map
-        for i, line in enumerate(self.yieldlines(token.map[1], env)):
-            if i == start:
-                print(
-                    "locals().update(__import__('midgy').load_front_matter(",
-                    file=pre,
-                    end="",
-                )
-                print(self.quote_char * 3, file=pre, end="")
-            if i == (stop - 1):
-                left = line.rstrip()
-                print(escape(left), file=pre, end="")
-                print(self.quote_char * 3, file=pre, end="")
-                print("))", file=pre, end="")
-                print(line[len(left) :], file=pre, end="")
-            else:
-                print(escape(line), file=pre, end="")
+        trail = self.quote_char * 3
+        lead = "locals().update(__import__('midgy').load_front_matter(" + trail
+        trail += "))"
+        body = "".join(self.yieldlines(token.map[1], env))
+        return self.compose_generic_string(body, env, lead=lead, trail=trail)
 
-        return pre.getvalue()
+
+def md_to_python(body, renderer_cls=Python, *, _renderers={}):
+    renderer = _renderers.get(renderer_cls)
+    if renderer is None:
+        renderer = _renderers[renderer_cls] = MarkdownIt(renderer_cls=renderer_cls)
+
+    return renderer.render(body)
